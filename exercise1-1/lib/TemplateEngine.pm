fe0da94a93aba94d3dde9e2076a0640d8ed0c79b
Shoya Ishimaru <he.was.born.in.pisa.in.1564@gmail.com>
2012-08-07 23:18:45 +0900

    文字コードのバグを修正

---
+++ exercise1-1/lib/TemplateEngine.pm
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+package TemplateEngine;
+use IO::File;
+use Encode;
+
+sub new {
+	my ($class, %values) = @_;
+	my $dataStructure ={file => $values{file}};
+	bless $dataStructure, $class;	
+}
+
+sub render {
+	my ($self, $insertText) = @_;
+	
+	#引数のデリファレンスと分割
+	my %insertText = %$insertText;
+    my $title = $insertText{"title"};
+    my $content = $insertText{"content"};
+
+	#HTMLエスケープ
+    $title = &escapeChars($title);
+    $content = &escapeChars($content);
+
+	#テンプレートファイルオープン
+    open(templateFileHundle, '<:utf8', $self ->{file}) or die "Cannot open ".$self->{file}." :".$!;
+	my @template = <templateFileHundle>;
+	close(templateFileHundle);
+
+	#テンプレートの置き換え
+	foreach my $line (@template){
+		$line =~ s/{%\s*title\s*%}/$title/g;
+		$line =~ s/{%\s*content\s*%}/$content/g;
+		print encode("utf8",$line);
+    }
+}
+
+sub escapeChars {
+	my $char = $_[0];
+	$char =~ s/</&lt;/g;
+	$char =~ s/</&gt;/g;
+	$char =~ s/"/&quot;/g;
+	$char =~ s/'/#39;/g;
+	$char =~ s/&/&amp;/g;
+}
+
+#何か返さないとエラー
+1;
